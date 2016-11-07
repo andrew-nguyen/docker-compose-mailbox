8ef3652cbccd3b86a7a1c51efee28f8175f2c3fb
Andrew Nguyen <andrew@na-consulting.net>
2016-11-06 21:35:20 -0800

    Add managesieve plugin support in RC

--- webserver/roundcube/config.inc.php
+++ webserver/roundcube/config.inc.php
@@ -71,7 +71,7 @@ $config['imap_auth_type'] = "CHECK";
 // PLUGINS
 // ----------------------------------
 // List of active plugins (in plugins/ directory)
-$config['plugins'] = array('archive', 'zipdownload', 'filesystem_attachments', 'hide_blockquote', 'password');
+$config['plugins'] = array('archive', 'zipdownload', 'filesystem_attachments', 'hide_blockquote', 'password', 'managesieve');
 
 // the default locale setting (leave empty for auto-detection)
 // RFC1766 formatted language name like en_US, de_DE, de_CH, fr_FR, pt_BR
